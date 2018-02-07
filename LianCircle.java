0c751419c62e4fe677baba08d809b6a6c4baba0d
lian199618 <36161530+lian199618@users.noreply.github.com>
2018-02-06 20:00:09 -0500

    Add files via upload

---
+++ LianCircle.java
@@ -0,0 +1,22 @@
+package PA03;
+
+import java.awt.*;
+import java.util.Random;
+
+public class LianCircle extends CircleShape {
+	Random rand = new Random();
+	int r = rand.nextInt(150);
+	
+	public LianCircle(){
+		super();
+		this.color = new Color(208,16,76,r+100);
+	}
+		
+	public void update(double dt){
+		this.x += dt*20*vx; 
+		this.y += dt*-5*vy;
+		
+		this.keepOnBoard();
+	}
+		 
+}
